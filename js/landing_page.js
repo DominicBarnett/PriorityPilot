26d42ef431979b0efc608991d9515355f1e8d1a8
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2025-02-04 22:56:15 -0800

    scroll effect updated

--- js/landing_page.js
+++ js/landing_page.js
@@ -4,6 +4,6 @@ let imageHero = document.querySelector('.image-hero');
 
 window.addEventListener('scroll', () => {
   var value = window.scrollY;
-  cloudRight.style.left = value * 0.5 + 'px';
-  cloudLeft.style.right = value * 0.5 + 'px';
+  cloudRight.style.left = value * 1 + 'px';
+  cloudLeft.style.right = value * 1 + 'px';
 });
