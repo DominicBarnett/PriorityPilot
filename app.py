5155f8149ee0f837fe0339a0a7436c59e4b7e7af
Ca'Sandra Smith <casresm3@gmail.com>
2025-02-05 16:07:31 -0500

    Created log in page base and added some base styling

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ app.config.from_pyfile('config.py')
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login-page.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
