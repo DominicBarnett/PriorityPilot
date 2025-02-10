f5855f06c4afaa90730312e1c46ba21f1ccf4f48
Ca'Sandra Smith <casresm3@gmail.com>
2025-02-10 12:09:17 -0500

    Added base for landing page info section

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app.config.from_pyfile('config.py')
 def index():
     return render_template('index.html')
 
+@app.route('/landing-page-info')
+def landing_age_info():
+    return render_template('landing-page-info.html')
+
 @app.route('/login')
 def login():
     return render_template('login-page.html')
