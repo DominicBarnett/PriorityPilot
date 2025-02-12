18bdfbe08127b240055397efb265fc46dd3f2c88
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2025-02-12 10:15:44 -0800

    updated comments

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-# from flask_pymongo import PyMongo
-# from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 # Load environment variables from a .env file if available
-# load_dotenv()
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "your_default_secret_key")
 app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb://localhost:27017/prioritypilotdev")
 
 # Initialize MongoDB connection
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
