791029a39333a8c0c9b453028cca71778690306d
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2025-02-26 15:08:27 -0800

    updated contact-us page

--- static/js/contact-us.js
+++ static/js/contact-us.js
@@ -1,7 +1,12 @@
+// Select all DOM elements that have class input from contact-us.js
 const inputs = document.querySelectorAll(".input");
 
+// When a user clicks or tabs into the input field
 function focusFunc() {
+  // this - refers to the input element that received focus.
+  // which retrieves the element's parent node and stores it in a variable
   let parent = this.parentNode;
+  // add CSS class  called "focus"
   parent.classList.add("focus");
 }
 
@@ -15,4 +20,11 @@ function blurFunc() {
 inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
+});
+
+// On page load, if an input has a value, add the "focus" class to its parent
+inputs.forEach((input) => {
+  if (input.value.trim() !== "") {
+    input.parentNode.classList.add("focus");
+  }
 });
