09afebf7ef75e4b91a351fc9afd09a9f3a7a5202
Ca'Sandra Smith <casresm3@gmail.com>
2025-03-01 21:04:34 -0500

    git rebase --continueMerging new main code into calender branch Merged main branch into calendar-page, resolved conflicts

--- app.py
+++ app.py
@@ -330,6 +330,10 @@ def landing():
 @app.route("/contact")
 def contact():
     return render_template('contact-us.html', active_page='contact')
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
